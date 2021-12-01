12eecef527fd6bb95142fe0b25e2b0b7a573183c
unknown <julioandresrivas@gmail.com>
2021-12-01 01:40:51 -0300

    commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,5 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
+const { stringify } = require("git-url-parse");
+
 // Crea una variable "string", puede contener lo que quieras:
 const nuevaString = "hola";
 
